871534337cf91ea4d41ce165399f37daf768da0b
yossu-y <yangyuub@gmail.com>
2022-05-03 06:22:18 +0000

    [add]controller-book_comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,18 +3,26 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    @book_comment = current_user.book_comments.new(book_comment_params)
-    @book_comment.book_id = @book.id
-    if @book_comment.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
-    else
-      redirect_to book_path(@book), notice: "comment can't be blank"
-    end
+    @book_comment = @book.comments.build(comment_params)
+    @book_comment.user_id = current_user.id
+    @book_comment.save
+    render book_path(params[:book_id])
+    # @book_comment = current_user.book_comments.new(book_comment_params)
+    # @book_comment.book_id = @book.id
+    # if @book_comment.save
+      # redirect_to book_path(@book), notice: "You have created book successfully."
+    # else
+      # redirect_to book_path(@book), notice: "comment can't be blank"
+    # end
+
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    # BookComment.find(params[:id]).destroy
+    # redirect_to book_path(params[:book_id])
+    @booke_comment = Comment.find(params[:id])
+    @book_comment.destroy
+    render book_path(params[:book_id])
   end
 
   private
@@ -22,7 +30,7 @@ class BookCommentsController < ApplicationController
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-  
+
   def correct_user
     @book_comment = BookComment.find(params[:id])
     @user = @book_comment.user
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @newbook = Book.new
     @book_comment = BookComment.new
+    @book_comments = @book.comments.order(created_at: :desc)
   end
 
   def index
